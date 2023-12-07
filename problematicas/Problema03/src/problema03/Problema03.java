b111d36b3d254ad1d3fa3dbb88eb66a4380a7bd5
tinobest1 <sayayinlenin@hotmail.com>
2023-12-07 16:56:49 -0500

    -axxxxxxxxx

--- problematicas/Problema03/src/problema03/Problema03.java
+++ problematicas/Problema03/src/problema03/Problema03.java
@@ -4,6 +4,9 @@
  */
 package problema03;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author LAB.ELECT
@@ -15,6 +18,24 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        //c = 5\9(f-32)
+        double f = 0, c = 0;
+        double operacion;
+        String mensajeFinal = "";
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Ingrese la cantidad de grados Celsius a "
+                    + "convertur");
+            c = entrada.nextDouble();
+            mensajeFinal = String.format("%sTabala %d\n",
+                    mensajeFinal); 
+            for (double contador = 4; contador <= 20; contador++) {
+                c = 5 / 9 * f - 32;
+            }
+            mensajeFinal = String.format("%s\n", mensajeFinal);
+        }
+            System.out.printf("%s\n", mensajeFinal);
+
     }
-    
 }
