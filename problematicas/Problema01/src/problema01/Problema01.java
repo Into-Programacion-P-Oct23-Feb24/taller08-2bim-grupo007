05eebccf34ecb6fb93ab544492a0158ddc15e207
Carlos <csvalladares2@utpl.edu.ec>
2023-12-07 16:03:20 -0500

    Ejemplo 01

--- problematicas/Problema01/src/problema01/Problema01.java
+++ problematicas/Problema01/src/problema01/Problema01.java
@@ -4,6 +4,9 @@
  */
 package problema01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author LAB.ELECT
@@ -14,7 +17,52 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.ITALY);
+
+        String nombre;
+        String posicion;
+        int edad;
+        double estatura;
+        double sumaEdades = 0;
+        double promedioEdades;
+        double sumaEstatura = 0;
+        double promedioEstatura;
+        int numero;
+        String acumulador = "Listado de Jugadores";
+        String edadesJuntas = "Listado de edades";
+
+        System.out.println("Ingrese el numero de veces que desea "
+                + "ingresar datos");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+        
+        for (int contador = 0; contador < numero; contador++) {
+            System.out.println("Ingrese el nombre del jugador");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese la posicion en el campo de juego");
+            posicion = entrada.nextLine();
+            System.out.println("Ingrese la edad del jugador");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del jugador");
+            estatura = entrada.nextDouble();
+            
+            sumaEdades = sumaEdades + edad;
+            sumaEstatura = (sumaEstatura + estatura);
+            
+            acumulador = String.format("%s%d. %s -%s-, edad %d"
+                    + ", estatura %.2f\n", acumulador, contador
+                    , nombre, posicion, edad,estatura);
+            
+           edadesJuntas = String.format("%s %d",edadesJuntas,edad);
+        }
+        promedioEdades = sumaEdades / numero;
+        promedioEstatura = sumaEstatura / numero;
+        
+        System.out.printf("%s",acumulador);
+        System.out.printf("%s",edadesJuntas);
+        System.out.printf("\nPromedio de edades: %.1f",promedioEdades);
+        System.out.printf(" Promedio de estaturas: %.2f", promedioEstatura);
     }
-    
+
 }
